3b9bf914ac81865487191b5fa68de816556e338a
Evan Winter <ewinter1996@gmail.com>
2017-05-30 19:40:08 -0500

    .

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'XWrauhbNSyksjZjuCkFJw36Da',
+	consumer_secret: '2Q4bx6BM8Q1ahw93zT3th9898R3HDFefC50CVH5AO1B9cJWB3m',
+	access_token_key: '3751591934-r87UIupgDVtWY47s6OXkVJ72qdqBvdXhXDaX8Fi',
+	access_token_secret: 'kSiQbcMplTM7bZb4yvQX7iCUiUtlmLMNtomyGKsASTQns'
+}
