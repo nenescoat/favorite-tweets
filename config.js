88706631e0fb4de868ad850fae72eb44b8f65f87
Evan Winter <ewinter1996@gmail.com>
2017-08-09 00:12:49 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key: '9ELlCdP4NJm7IF9MdiQj0zSl1',
-	consumer_secret: 'jLPHY4RO8ZLVgHfZWyD4YJlahclUzqIP4pjm6wxG8G8OpmYfip',
-	access_token_key: '879877809367392256-LEpG9GAm2U3JNb0v6lXsQEGNijvAl9M',
-	access_token_secret: 'UCA09oOK0CEanVnEwEZ0CzghJ4lLWrCki3n8nIyBQiMeo'
-}
