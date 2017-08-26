23271ed06ac394dc6e169d7037e52fb079c8e265
nenescoat <31359512+nenescoat@users.noreply.github.com>
2017-08-26 16:36:51 -0500

    re

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+module.exports = {
+	consumer_key: 'ihfzAcSqoRjLrX7Kli3n7Ji7Y',
+	consumer_secret: 'W1IsBCs7YcKOs5L918EfHxlq2f301dMCaT1zRIJ80dntyUrFOX',
+	access_token_key: '720308220477059072-UwW59EXotWHQ73bSMnlOm2Kh5ZHAyKG',
+	access_token_secret: 'Z3aN6NfWcOg0s48iDgAluW36KMGzCs0DBmfq1wBbqYRDy'
+}
+npm i
+
+node app.js
